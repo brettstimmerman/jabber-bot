5d94d0878b397e23327470446569d33109d72f55
Brett Stimmerman <brettstimmerman@gmail.com>
2007-03-21 02:04:10 +0000

    Public options for bot and commands; support multiple masters

--- jabber-bot/sample/sample.rb
+++ jabber-bot/sample/sample.rb
@@ -2,41 +2,45 @@
 require 'rubygems'
 require 'jabber/bot'
 
-# Configure the bot
-jabber_id = 'bot@example.com'
-password  = 'password'
-master_id = 'master@example.com'
+# Configure a public bot
+bot_config = {
+  :jabber_id => 'bot@example.com',
+  :password  => 'password',
+  :master    => 'master@example.com',
+  :is_public => true
+}
 
 # Create a new bot
-bot = Jabber::Bot.new(jabber_id, password, master_id)
+bot = Jabber::Bot.new(bot_config)
 
-# Give the bot a new command, 'puts', with a response message
+# Give the bot a private command, 'puts', with a response message
 bot.add_command(
-  :command     => 'puts <string>',
+  :syntax      => 'puts <string>',
   :description => 'Write something to $stdout',
   :regex       => /^puts\s+.+$/
-) do |message|
+) do |sender, message|
   puts message
   "'#{message}' written to $stdout"
 end
 
-# Give the bot another command, 'puts!' without a response message
+# Give the bot another private command, 'puts!', without a response message
 bot.add_command(
-  :command     => 'puts! <string>',
+  :syntax      => 'puts! <string>',
   :description => 'Write something to $stdout',
   :regex       => /^puts!\s+.+$/
-) do |message|
-  puts message
+) do |sender, message|
+  puts "#{sender} says '#{message}'"
   nil
 end
 
-# Give the bot another command, 'rand', with alias 'r'
+# Give the bot a public command, 'rand', with alias 'r'
 bot.add_command(
-  :command     => 'rand',
+  :syntax      => 'rand',
   :description => 'Produce a random number from 0 to 10',
   :regex       => /^rand$/,
-  :aliases     => [:alias => 'r', :regex => /^r$/]
+  :alias       => [:syntax => 'r', :regex => /^r$/],
+  :is_public      => true
 ) { rand(10).to_s }
 
-# Unleash the new bot
+# Unleash the bot
 bot.connect
